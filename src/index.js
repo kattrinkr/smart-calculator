cba98c7a66fd7dac754543f998d52ffbba961794
kattrinkr <kattrinkr@mail.ru>
2018-03-06 20:59:02 +0300

    feature|logic of calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initial = initialValue;// your implementation
   }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
+  add(number) {// Сложение
+    this.initial = this.initial + number;
+    this.initial.valueOf();
+    return this;
   }
 
-  multiply(number) {
-    // your implementation
+  subtract(number) {// Вычитание
+    this.initial = this.initial - number;
+    this.initial.valueOf();
+    return this;
   }
 
-  devide(number) {
-    // your implementation
+  multiply(number) {// Умножение
+    this.initial = this.initial * number;
+    this.initial.valueOf();
+    return this;
   }
 
-  pow(number) {
-    // your implementation
+  devide(number) {// Деление
+    if (number !== 0) {
+      this.initial = Math.round(this.initial / number);
+      this.initial.valueOf();
+      return this;
+    }
+    else return false;
   }
+
+  pow(number) {// Степень
+    for (var i = 1; i <= number; i++)
+      this.initial = this.initial * this.initial;
+      this.initial.valueOf();
+      return this;
+  }
+
+  valueOf(){
+    return this.initial;
+  }
+
+
 }
 
 module.exports = SmartCalculator;
