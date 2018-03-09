d16ccd2e454ce3b820bf511f6a8ee57eab8b0a4e
kattrinkr <kattrinkr@mail.ru>
2018-03-09 21:06:25 +0300

    feature|new logic of calculator

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 class SmartCalculator {
   constructor(initialValue) {
-    this.initial = initialValue;// your implementation
+    this.initial = initialValue;
+    this.expression = [];
+    this.expression.push(initialValue);// your implementation
   }
 
   add(number) {// Сложение
-    this.initial = this.initial + number;
+    this.sumExpression("+", number)
+    this.initial = this.solve();
     this.initial.valueOf();
     return this;
+
   }
 
   subtract(number) {// Вычитание
-    this.initial = this.initial - number;
+    this.sumExpression("-", number)
+    this.initial = this.solve();
     this.initial.valueOf();
     return this;
   }
 
   multiply(number) {// Умножение
-    this.initial = this.initial * number;
+    this.sumExpression("*", number);
+    this.initial = this.solve();
     this.initial.valueOf();
     return this;
   }
 
   devide(number) {// Деление
+    this.sumExpression("/", number);
     if (number !== 0) {
-      this.initial = Math.round(this.initial / number);
+      this.initial = this.solve(); //Math.round(this.initial / number);
       this.initial.valueOf();
       return this;
     }
@@ -31,17 +38,85 @@ class SmartCalculator {
   }
 
   pow(number) {// Степень
+    this.sumExpression("**", number);
     for (var i = 1; i <= number; i++)
-      this.initial = this.initial * this.initial;
+      this.initial = this.solve();
       this.initial.valueOf();
       return this;
   }
 
-  valueOf(){
+  sumExpression (operation, num) {
+    this.expression.push(operation, num);
+  //  console.log(this.expression);
+  }
+
+  solve () {
+    var i = 1, tempanswer = 0;
+    var temp = [];
+    for (var j = 0; j < this.expression.length; j++){
+      temp.push(this.expression[j]);
+    }
+  //  console.log(temp);
+    while (temp.length !== 1){
+      if (temp[i] === "**"){
+        tempanswer = Math.pow(temp[i-1],temp[i+1]);
+        temp.splice(i-1, 3, tempanswer);
+        i = i - 2;
+      }//console.log(temp);
+      i++;
+      if (i === temp.length-1) break;
+    }
+    i = 1;
+    while (temp.length !== 1){
+      if (temp[i] === "/"){
+        tempanswer = Math.round(temp[i-1] / temp[i+1]);
+        temp.splice(i-1, 3, tempanswer);
+        i = i - 2;
+      }
+      i++;
+      if (i === temp.length-1) break;
+    }
+    i = 1;
+    while (temp.length !== 1){
+      if (temp[i] === "*"){
+        tempanswer = temp[i-1] * temp[i+1];
+        temp.splice(i-1, 3, tempanswer);
+        i = i - 2;
+      }
+      i++;
+      if (i === temp.length-1) break;
+    }//console.log(temp);
+
+    i = 1;
+    while (temp.length !== 1){
+      if (temp[i] === "-"){
+        tempanswer = temp[i-1] - temp[i+1];
+        temp.splice(i-1, 3, tempanswer);
+        i = i - 2;
+      }
+      i++;
+      if (i === temp.length-1) break;
+    }
+    i = 1;
+    while (temp.length !== 1){
+      if (temp[i] === "+"){
+        tempanswer = temp[i-1] + temp[i+1];
+        temp.splice(i-1, 3, tempanswer);
+        i = i - 2;
+      }
+      i++;
+      if (i === temp.length-1) break;
+    }//console.log(temp);
+
+    return temp[0];
+  }
+
+  valueOf() {
     return this.initial;
   }
 
-
 }
+
+
 
 module.exports = SmartCalculator;
