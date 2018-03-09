3ec38e2161e96ea01b4d6bf59265ec5c9d1bb783
kattrinkr <kattrinkr@mail.ru>
2018-03-09 21:29:36 +0300

    feature|add coments

--- temp.js
+++ temp.js
@@ -8,7 +8,7 @@ class SmartCalculator {
   add(number) {// Сложение
     this.sumExpression("+", number)
     this.initial = this.solve();
-    this.initial.valueOf();
+    //this.initial.valueOf();
     return this;
 
   }
@@ -16,22 +16,22 @@ class SmartCalculator {
   subtract(number) {// Вычитание
     this.sumExpression("-", number)
     this.initial = this.solve();
-    this.initial.valueOf();
+  //  this.initial.valueOf();
     return this;
   }
 
   multiply(number) {// Умножение
     this.sumExpression("*", number);
     this.initial = this.solve();
-    this.initial.valueOf();
+  //  this.initial.valueOf();
     return this;
   }
 
   devide(number) {// Деление
     this.sumExpression("/", number);
     if (number !== 0) {
-      this.initial = this.solve();
-      this.initial.valueOf();
+      this.initial = this.solve(); //Math.round(this.initial / number);
+    //  this.initial.valueOf();
       return this;
     }
     else return false;
@@ -41,34 +41,34 @@ class SmartCalculator {
     this.sumExpression("**", number);
     for (var i = 1; i <= number; i++)
       this.initial = this.solve();
-      this.initial.valueOf();
+  //    this.initial.valueOf();
       return this;
   }
 
-  sumExpression (operation, num) {//Пощаем операцию и число в массив
+  sumExpression (operation, num) {
     this.expression.push(operation, num);
+    console.log(this.expression);
   }
 
   solve () {
     var temp = [];
-    for (var j = 0; j < this.expression.length; j++) {//Помещаем во временный массив
-      temp.push(this.expression[j]);                 //  все выражение
+    for (var j = 0; j < this.expression.length; j++){
+      temp.push(this.expression[j]);
     }
-
+    console.log(temp);
     var i = temp.length-1, tempanswer = 0;
-    while (temp.length !== 1) {//С конца массива ищем знак степени(Приоритет 1)
-      if (temp[i] === "**") {
+    while (temp.length !== 1){
+      if (temp[i] === "**"){
         tempanswer = Math.pow(temp[i-1],temp[i+1]);
-        temp.splice(i-1, 3, tempanswer);//Удаление из массива 3х элементов
-        i = i + 2;                      //  с позиции i-1, замена первого
-      }                                 //  элемента на tempanswer
-    i--;
-    if (i === 0) break;
+        temp.splice(i-1, 3, tempanswer);
+        i = i + 2;
+      }console.log(temp);
+      i--;
+      if (i === 0) break;
     }
-
     i = 1;
-    while (temp.length !== 1) {//С начала массива ищем знак деления(Приоритет 2)
-      if (temp[i] === "/") {
+    while (temp.length !== 1){
+      if (temp[i] === "/"){
         tempanswer = Math.round(temp[i-1] / temp[i+1]);
         temp.splice(i-1, 3, tempanswer);
         i = i - 2;
@@ -76,21 +76,20 @@ class SmartCalculator {
       i++;
       if (i === temp.length-1) break;
     }
-
     i = 1;
-    while (temp.length !== 1) {//С начала массива ищем знак умножения(Приоритет 3)
-      if (temp[i] === "*") {
+    while (temp.length !== 1){
+      if (temp[i] === "*"){
         tempanswer = temp[i-1] * temp[i+1];
         temp.splice(i-1, 3, tempanswer);
         i = i - 2;
       }
       i++;
       if (i === temp.length-1) break;
-    }
+    }console.log(temp);
 
     i = 1;
-    while (temp.length !== 1) {//С начала массива ищем знак вычитания(Приоритет 4)
-      if (temp[i] === "-") {
+    while (temp.length !== 1){
+      if (temp[i] === "-"){
         tempanswer = temp[i-1] - temp[i+1];
         temp.splice(i-1, 3, tempanswer);
         i = i - 2;
@@ -98,23 +97,68 @@ class SmartCalculator {
       i++;
       if (i === temp.length-1) break;
     }
-
     i = 1;
-    while (temp.length !== 1) {//С начала массива ищем знак суммы(Приоритет 5)
-      if (temp[i] === "+") {
+    while (temp.length !== 1){
+      if (temp[i] === "+"){
         tempanswer = temp[i-1] + temp[i+1];
         temp.splice(i-1, 3, tempanswer);
         i = i - 2;
       }
       i++;
       if (i === temp.length-1) break;
-    }
-    return temp[0];
+    }console.log(temp);
+
+    console.log(temp);
   }
+
+  priority(temp) {
+  }
+
+  answer(output){
+}
 
   valueOf() {
     return this.initial;
   }
 
 }
-module.exports = SmartCalculator;
+
+const calculator = new SmartCalculator(7);
+const value = calculator
+/* .add(5)
+   .add(5)
+  .multiply(5)
+  .add(5);*/
+/* .add(50)
+  .multiply(2)
+  .add(44)
+  .pow(1)
+  .add(5)
+  .multiply(1)
+  .multiply(2);*/
+/*  .pow(2)
+  .multiply(1)
+  .pow(1)
+  .multiply(1); */
+/* .add(2)
+  .add(2)
+  .multiply(2);*/
+//  .add(5);
+/*  .multiply(1)
+  .pow(1)
+  .subtract(8)
+  .add(84)
+  .subtract(12)
+  .add(20)
+  .subtract(95)
+  .subtract(72)
+  .multiply(2)
+  .subtract(77);*/
+  .pow(1)
+  .subtract(25)
+  .subtract(40)
+  .pow(1)
+  .pow(2)
+  .pow(2)
+  .multiply(1)
+  .subtract(9);
